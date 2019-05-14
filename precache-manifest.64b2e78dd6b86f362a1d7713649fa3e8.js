194cfebb092ad5d1eb1a87434b0629197573696d
avalos010 <luizavalos40@gmail.com>
2019-05-13 19:51:28 -0500

    Updates

---
+++ precache-manifest.64b2e78dd6b86f362a1d7713649fa3e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3af07cd0851b3939b05452e82b5fa2cf",
+    "url": "/movie-search/index.html"
+  },
+  {
+    "revision": "4c9187734990470a1375",
+    "url": "/movie-search/static/css/main.66585ef3.chunk.css"
+  },
+  {
+    "revision": "79a8598ae72867d7869c",
+    "url": "/movie-search/static/js/2.677c9b9b.chunk.js"
+  },
+  {
+    "revision": "4c9187734990470a1375",
+    "url": "/movie-search/static/js/main.4bc230cd.chunk.js"
+  },
+  {
+    "revision": "467f242a197283b43b88",
+    "url": "/movie-search/static/js/runtime~main.5125b6b9.js"
+  },
+  {
+    "revision": "3fe2d55dcd9d19ec7c319a856852bc25",
+    "url": "/movie-search/static/media/background_search.3fe2d55d.jpg"
+  }
+]);
